427a44da00f6f2123d518997ed9f89ac137ab50d
Rishita Raha <64982040+rishita2605@users.noreply.github.com>
2023-12-23 21:32:04 +0530

    [chore] update prettier config

--- src/components/sign-up/sign-up.tsx
+++ src/components/sign-up/sign-up.tsx
@@ -1,53 +1,73 @@
-import React, { useState, type ChangeEvent } from 'react'
-import { SignUpFormFields, SignUpFormType } from '.'
-import { Button } from '../../shared/button/button'
-import { Input, type FormFieldsType } from '../../shared/input'
+import React, { useState, type ChangeEvent } from "react"
+import { SignUpFormFields, SignUpFormType } from "."
+import { Button } from "../../shared/button/button"
+import { Input, type FormFieldsType } from "../../shared/input"
+import { useNavigate } from "react-router-dom"
 
 const initialInputState: SignUpFormType = {
   userName: {
-    value: '',
-    error: undefined
+    value: "",
+    error: undefined,
   },
   password: {
-    value: '',
-    error: undefined
-  }
+    value: "",
+    error: undefined,
+  },
 }
 
 export const SignUp: React.FC = () => {
   // States.
   const [formValues, setFormValues] = useState<SignUpFormType>(initialInputState)
+  const navigate = useNavigate()
 
   // Handlers.
   const onSubmit = (event: any): void => {
     event.preventDefault()
+    if (
+      formValues.userName.value !== process.env.REACT_APP_USERNAME ||
+      formValues.password.value !== process.env.REACT_APP_PASSWORD
+    ) {
+      setFormValues((prevValues) => {
+        return {
+          ...prevValues,
+          [SignUpFormFields.password]: {
+            ...prevValues[SignUpFormFields.password],
+            error: "Uh oh! Invalid username or password.",
+          },
+        }
+      })
+    } else {
+      navigate("/products")
+    }
   }
 
   const formChange = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    field: SignUpFormFields
+    field: SignUpFormFields,
   ): void => {
     setFormValues((prevValue: SignUpFormType) => {
       const res: FormFieldsType = {
         value: event.target.value,
-        error: undefined
+        error: undefined,
       }
 
       return {
         ...prevValue,
-        [field]: res
+        [field]: res,
       }
     })
   }
 
   return (
-    <div className='sign-up'>
-      <form className='sign-up__form'>
-        <div className='sign-up__form__header'>Hey there, it’s good to see you again!</div>
+    <div className="sign-up">
+      <form className="sign-up__form">
+        <div className="sign-up__form__header">
+          Hey there, it’s good to see you again!
+        </div>
         <Input
-          type='text'
-          placeholder='Please enter your username'
-          label='Username.'
+          type="text"
+          placeholder="Please enter your username"
+          label="Username."
           error={formValues.userName.error}
           value={formValues.userName.value}
           onChange={(event) => {
@@ -57,9 +77,9 @@ export const SignUp: React.FC = () => {
         />
 
         <Input
-          type='password'
-          placeholder='Please enter your password'
-          label='Password.'
+          type="password"
+          placeholder="Please enter your password"
+          label="Password."
           error={formValues.password.error}
           value={formValues.password.value}
           onChange={(event) => {
