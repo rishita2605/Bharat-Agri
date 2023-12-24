51e2f21af6dd8104f9c6f8aa99fbd034d17a368b
Rishita Raha <64982040+rishita2605@users.noreply.github.com>
2023-12-24 23:44:31 +0530

    [fix] linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import "./index.scss"
-import reportWebVitals from "./reportWebVitals"
 import { Products, SignUp } from "./components"
+import "./index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root")!)
 
